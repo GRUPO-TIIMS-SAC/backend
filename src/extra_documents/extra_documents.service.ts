29ea4f6763070bde9127c5d3905da61b3acf3782
CxG346 <dgm3036@gmail.com>
2024-02-10 20:01:18 -0500

    Edit endpoints

--- src/extra_documents/extra_documents.service.ts
+++ src/extra_documents/extra_documents.service.ts
@@ -14,6 +14,8 @@ import * as multer from 'multer';
 import { Request } from 'express';
 import { FilesService } from 'src/files/files.service';
 import { ExtraDocumentsListService } from 'src/extra_documents_list/extra_documents_list.service';
+import { DeleteFileDto } from 'src/files/dto/delete-file.dto';
+import { Utils } from 'src/utils/utils';
 
 @Injectable()
 export class ExtraDocumentsService {
@@ -34,7 +36,7 @@ export class ExtraDocumentsService {
 
     try {
       //VALID BODY
-      if(body.document_id != 'cv' && body.document_id != 'certi-joven'){
+      if (body.document_id != 'cv' && body.document_id != 'certi-joven') {
         return new HttpException(
           { message: 'Document type invalid' },
           HttpStatus.NOT_FOUND,
@@ -81,11 +83,20 @@ export class ExtraDocumentsService {
         },
       });
 
+      // if (existDocument) {
+      //   return new HttpException(
+      //     { message: 'Document already exists' },
+      //     HttpStatus.CONFLICT,
+      //   );
+      // }}
+
       if (existDocument) {
-        return new HttpException(
-          { message: 'Document already exists' },
-          HttpStatus.CONFLICT,
-        );
+        const body: DeleteFileDto = {
+          dir: 'documents_upload',
+          file: existDocument.url,
+        };
+
+        await this.filesService.deleteStorageFile(body);
       }
 
       //UPLOAD FILE
@@ -118,7 +129,7 @@ export class ExtraDocumentsService {
       const newExtraDocument = this.extraDocumentRepository.create({
         document_id: body.document_id === 'cv' ? 1 : 2,
         user_id: tokenDecoded.id,
-        url: 'documents_upload/' + fileName,
+        url: fileName,
       });
 
       const respData =
@@ -132,6 +143,64 @@ export class ExtraDocumentsService {
       console.log(error);
       return new HttpException(
         { message: 'Error creating extra document', error: error.message },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async upload(token: any, req: multer.File, body: createExtraDocument) {
+    try {
+      //VALID BODY
+      if (body.document_id != 'cv' && body.document_id != 'certi-joven') {
+        return new HttpException(
+          { message: 'Document type invalid' },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      //TOKEN
+      const tokenDecoded = this.userService.decodeToken(token);
+      let fileName;
+      let documentName;
+
+      if (!tokenDecoded.id) {
+        return new HttpException(
+          { message: 'Token wrong' },
+          HttpStatus.CONFLICT,
+        );
+      }
+      //USER
+      const user = await this.userService.findOne(tokenDecoded.id);
+      if (user.getStatus() === 404) {
+        return new HttpException(
+          { message: 'User not found' },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      //LIST DOCUMENTS
+      const listDocuments = await this.extraDocumentListService.getOne(
+        body.document_id === 'cv' ? 1 : 2,
+      );
+
+      if (listDocuments.getStatus() != 200) {
+        return listDocuments;
+      }
+
+      documentName = listDocuments.getResponse()['data'].document;
+
+      //VALIDATED DUPLICATE DOCUMENT
+      const existDocument = await this.extraDocumentRepository.findOne({
+        where: {
+          user_id: tokenDecoded.id,
+          document_id: body.document_id === 'cv' ? 1 : 2,
+        },
+      });
+
+      if (existDocument) {
+      }
+    } catch (error) {
+      return new HttpException(
+        { message: 'Error updating extra document', error: error.message },
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
@@ -170,7 +239,14 @@ export class ExtraDocumentsService {
       }
 
       return new HttpException(
-        { data: extraDocuments, message: 'Extra documents found' },
+        {
+          data: extraDocuments.map((extraDocument) => {
+            extraDocument.url = new Utils().route() + '/documents_upload/' + extraDocument.url;
+            return extraDocument;
+          }),
+
+          message: 'Extra documents found',
+        },
         HttpStatus.OK,
       );
     } catch (error) {
