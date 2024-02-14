13d3feff4e4045f7209087c92d9fcf1e63bda62f
CxG346 <dgm3036@gmail.com>
2024-02-14 17:09:29 -0500

    add delete and get by specilities services

--- src/services/services.controller.ts
+++ src/services/services.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Headers, Body} from '@nestjs/common';
+import { Controller, Post, Headers, Body, Get, Param, ParseIntPipe, Delete} from '@nestjs/common';
 import { ServicesService } from './services.service';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { ApiTags } from '@nestjs/swagger';
@@ -16,5 +16,14 @@ export class ServicesController {
         return this.servicesService.create(token, body);
     }
 
+    @Get('speciality/:id')
+    async getServicesBySpeciality(@Param('id', ParseIntPipe) id: number){
+        return this.servicesService.getBySpeciality(id);
+    }
+
+    @Delete(':id')
+    async deleteService(@Headers('authorization') token: any,@Param('id', ParseIntPipe) id: number){
+        return this.servicesService.delete(token, id);
+    }
     
 }
